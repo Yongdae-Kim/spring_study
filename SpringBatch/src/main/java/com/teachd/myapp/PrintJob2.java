504a5f229ff76b2a5241ac12776c3d766a289948
daum <daum@aptest.local>
2015-09-10 15:21:01 +0900

    스프링 배치 샘플 추가

---
+++ SpringBatch/src/main/java/com/teachd/myapp/PrintJob2.java
@@ -0,0 +1,31 @@
+package com.teachd.myapp;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+
+// ref http://www.jroller.com/0xcafebabe/entry/spring_batch_hello_world_1
+
+public class PrintJob2 implements Tasklet {
+	private String message;
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public RepeatStatus execute(StepContribution arg0, ChunkContext arg1)
+			throws Exception {
+		System.out.println("#################### JOB 2 ####################");
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Date date = new Date();
+		System.out.println(dateFormat.format(date));
+		System.out.println(message);
+		System.out.println("#################### END 2 ####################");
+		return RepeatStatus.FINISHED;
+	}
+}
